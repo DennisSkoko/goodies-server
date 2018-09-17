bc33d839789fc93a47bf662077a0123ee6dbd40a
Dennis Skoko <dennis.skoko@hotmail.com>
2018-09-17 20:41:22 +0200

    Adds support for configuring env variables

---
+++ src/config.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env')
+})
--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('./config')
+
 const { ApolloServer } = require('apollo-server-lambda')
 const { createConfigFromModels } = require('./util/graphql')
 const models = require('./models')
