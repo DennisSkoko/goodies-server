620c392f81d99a61900be3138189406a93991eeb
Dennis Skoko <dennis.skoko@hotmail.com>
2018-09-21 07:47:57 +0200

    Updates graphql configuration

--- src/graphql.js
+++ src/graphql.js
@@ -6,8 +6,13 @@ const { ApolloServer } = require('apollo-server-lambda')
 const { createConfigFromModels } = require('./util')
 const models = require('./models')
 
+const IS_PROD = process.env.NODE_ENV === 'production'
+
 const server = new ApolloServer({
-  ...createConfigFromModels(models)
+  ...createConfigFromModels(models),
+  playground: !IS_PROD,
+  debug: !IS_PROD,
+  tracing: !IS_PROD
 })
 
 module.exports.handler = server.createHandler()
